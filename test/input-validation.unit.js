e8a4a1b244d379977513162f3483913055153bd0
Ryan Kotzen <Ryank@entelect.co.za>
2015-10-13 18:30:52 +0200

    populating test data.
    updating tests to be async.

--- test/input-validation.unit.js
+++ test/input-validation.unit.js
@@ -4,21 +4,33 @@ var expect = chai.expect;
 var _ = require('lodash');
 
 var defaultOptions = {
-    programName: "",
-    clientId: "",
-    username: "",
-    password: "",
-    redirectUri: ""
+    programName: "vivo-admin",
+    clientId: "cairo-frontend-dev",
+    username: "vivoenergy@encentivize.co.za",
+    password: "123456",
+    redirectUri: "http://localhost:6792/#/login"
 };
 
 describe('The get token method should validate the inputs that it is passed', function () {
     "use strict";
-    it('#01 - Program name is required', function () {
+    it('#01 - Program name is required', function (callback) {
         var options = _.clone(defaultOptions);
         delete options.programName;
         getToken(options, getTokenComplete);
         function getTokenComplete(err) {
+            expect(err).to.be.ok;
             expect(err.message).to.equal(getToken.messages.programNotFound);
+            callback();
+        }
+    });
+    it('#02 - Client Id is required', function (callback) {
+        var options = _.clone(defaultOptions);
+        delete options.clientId;
+        getToken(options, getTokenComplete);
+        function getTokenComplete(err) {
+            expect(err).to.be.ok;
+            expect(err.message).to.equal(getToken.messages.programNotFound);
+            callback();
         }
     });
 });
